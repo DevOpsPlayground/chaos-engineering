06db9ad50498f040d2a024c98dffdf7c78929b36
Simon Hanmer <simon.hanmer@gmail.com>
2024-10-28 11:04:49 +0000

    add outgoing http rule to sg

--- steps/modules/vpc/sg.tf
+++ steps/modules/vpc/sg.tf
@@ -1,9 +1,18 @@
 resource "aws_security_group" "ec2_sg" {
-  name        = "step01_${var.resource_suffix}_sg"
+  name        = "${var.resource_suffix}_sg"
   description = "Allow inbound traffic on port 80"
   vpc_id      = aws_vpc.this.id
 }
 
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_sg" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+  referenced_security_group_id = aws_security_group.ec2_sg.id
+}
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.ec2_sg.id
@@ -23,11 +32,20 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   cidr_ipv4   = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_https_connect" {
+resource "aws_vpc_security_group_egress_rule" "allow_https" {
   security_group_id = aws_security_group.ec2_sg.id
 
   from_port   = 443
   to_port     = 443
   ip_protocol = "tcp"
   cidr_ipv4   = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_http_sg" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+  referenced_security_group_id = aws_security_group.ec2_sg.id
 }
